fc780de8a28151ab4fbc4f9ac34b6429a50db013
n1tjrgns <n2tjrgns@gmail.com>
2020-02-16 15:12:37 +0900

    Feature/3vs500-1 Initial Commit

---
+++ src/main/java/com/service/applehip/ApplehipApplication.java
@@ -0,0 +1,13 @@
+package com.service.applehip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplehipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplehipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/service/applehip/ApplehipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.applehip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplehipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
