7e5469fb13d2bc65dc04378615ca5bef88344a4d
hil00137 <hil00137@gmail.com>
2020-02-29 00:41:44 +0900

    ChatService 클래스 추가, 주석 추가, 테스트 추가. refs #20

--- src/main/java/com/service/applehip/web/dto/chat/ChatSend.kt
+++ src/main/java/com/service/applehip/web/dto/chat/ChatSend.kt
@@ -1,11 +1,17 @@
 package com.service.applehip.web.dto.chat
 
+/**
+ * 채팅발송 요청 DTO
+ */
 class ChatSendRequestDto(
         var roomId : Int,
         var msg : String,
         var requestId: Int
 )
 
+/**
+ * 채팅발송 결과 DTO
+ */
 class ChatSendResponseDto(
         var result : Boolean,
         var detailMsg : String
--- src/test/java/com/service/applehip/web/api/ChatApiControllerTest.kt
+++ src/test/java/com/service/applehip/web/api/ChatApiControllerTest.kt
@@ -9,6 +9,7 @@ import org.json.JSONObject
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.web.server.LocalServerPort
@@ -27,12 +28,17 @@ class ChatApiControllerTest {
     // graphql 은 고정 url
     private fun getUrl() = "http://localhost:$port/graphql"
 
+    @Value(value = "\${chat.queue.size}")
+    private lateinit var queueSize : String
+    @Value(value = "\${chat.queue.prefix}")
+    private lateinit var queuePrefix : String
+
     @Test
     fun 기본_URL_테스트() {
         //given
-        val roomId = Random.nextInt()
+        val roomId = Random.nextInt(0,10000000)
         val msg = "This is a 테스트 메시지"
-        val requestId = Random.nextInt()
+        val requestId = Random.nextInt(0,1000000)
         val requestDto = ChatSendRequestDto(
                 roomId = roomId,
                 msg = msg,
@@ -64,7 +70,49 @@ class ChatApiControllerTest {
                            ") {" +
                            "    result,detailMsg}" +
                            "}"
+        println("query:$query")
+        // .also는 also안에서 it을 이용하여 해당 객체를 조작한뒤 다시 해당 객체를 반환함.
+        // Kotlin에서는 new를 생략할수있음
+        val queryJson = JSONObject().also { it.put("query", query) }
+        val httpHeaders = HttpHeaders().also { it.contentType = MediaType.APPLICATION_JSON_UTF8 }  // json 형식으로 던져야함
+        val httpEntity : HttpEntity<String> = HttpEntity(queryJson.toString(), httpHeaders) //header와 함께 보내기 위함.
 
+        //when
+        val responseEntity: ResponseEntity<String> = restTemplate.postForEntity(this.getUrl(), httpEntity, String::class.java)
+
+        //then
+        Assertions.assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)   // http 통신 OK
+        val body = responseEntity.body
+        println(body)
+        MatcherAssert.assertThat(body, IsNull.notNullValue()) // return value가 null이면 안됨
+
+        val dataJson = JSONObject(body).get("data") as JSONObject // data라는 이름으로 고정됨
+        MatcherAssert.assertThat(dataJson.get(queryName), IsNull.notNullValue()) //data안에 query이름과 같은 결과값이 존재
+        val response = dataJson.get(queryName) as JSONObject
+        val queueNo = roomId % queueSize.toInt()    // ChatService안의 로직과 동일하게 구현
+        if(queueNo < queueSize.toInt()/2) {
+            MatcherAssert.assertThat(response["result"] as Boolean, Matchers.equalTo(true))
+        } else {
+            MatcherAssert.assertThat(response["result"] as Boolean, Matchers.equalTo(false))
+        }
+        MatcherAssert.assertThat(response["detailMsg"] as String, Matchers.equalTo(queuePrefix+"_"+queueNo))
+    }
+
+    @Test
+    fun 큐사이즈_가져오기() {
+        //given
+        val queryName = "getQueueSize"
+
+        /* query 설정
+
+            query {
+                getQueueSize
+            }
+
+         */
+        val query = "query {" +          // 데이터를 변경하는 작업은 mutation 이 들어감
+                "    $queryName }"
+        println("query:$query")
         // .also는 also안에서 it을 이용하여 해당 객체를 조작한뒤 다시 해당 객체를 반환함.
         // Kotlin에서는 new를 생략할수있음
         val queryJson = JSONObject().also { it.put("query", query) }
@@ -77,12 +125,11 @@ class ChatApiControllerTest {
         //then
         Assertions.assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)
         val body = responseEntity.body
+        println(body)
         MatcherAssert.assertThat(body, IsNull.notNullValue()) // return value가 null이면 안됨
 
         val dataJson = JSONObject(body).get("data") as JSONObject
         MatcherAssert.assertThat(dataJson.get(queryName), IsNull.notNullValue())
-        val response = dataJson.get(queryName) as JSONObject
-        MatcherAssert.assertThat(response["result"] as Boolean, Matchers.equalTo(false))
-        MatcherAssert.assertThat(response["detailMsg"] as String, Matchers.equalTo(""))
+        MatcherAssert.assertThat(dataJson.get(queryName) as Int, Matchers.equalTo(queueSize.toInt()))
     }
 }
