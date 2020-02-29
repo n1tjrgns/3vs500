7a89e0f706b924ac645bf73f504a7dd5ea6a093b
hil00137 <hil00137@gmail.com>
2020-03-01 00:59:45 +0900

    ActiveMQ insert 클래스 작성. fixed #20 실제 ActiveMq insert class작성 관련 테스트 케이스로 변경

--- src/main/java/com/service/applehip/service/chat/ChatService.kt
+++ src/main/java/com/service/applehip/service/chat/ChatService.kt
@@ -13,14 +13,24 @@ class ChatService(private val chatSender: ChatSender) {
     @Value(value = "\${chat.queue.prefix}")
     private lateinit var queuePrefix : String
 
-    fun sendMsg(request: ChatSendRequestDto) : ChatSendResponseDto {
-        val queueNo = request.roomId % queueSize.toInt()
-        return if(queueNo < queueSize.toInt()/2) {
-            ChatSendResponseDto(true, queuePrefix+"_"+queueNo)
-        } else {
-            ChatSendResponseDto(false, queuePrefix+"_"+queueNo)
-        }
-    }
+    /**
+     * 함수가 길지않은경우에는 아래와같이 할수있음
+     * fun 지금함수명() = 다른함수()
+     * 위의 경우 다른함수의 return 타입이 지금함수의 return 타입으로 결정됨
+     * fun 함수명() = 상수
+     */
+    fun sendMsg(request: ChatSendRequestDto)
+            = chatSender.sendMsg(queueName = this.getQueName(request.roomId), request = request)
 
+    /**
+     * 큐사이즈를 int 형으로 반환
+     */
     fun getQueueSize() = queueSize.toInt()
+
+    /**
+     * queue 이름 생성규칙 prefix_[0 부터 queueSize-1]
+     * 같은 채팅방번호일경우 같은 큐에 쌓임, 하지만 단일큐는 많이 처리할수 없음.
+     * 큐사이즈를 변경하려면 properties 에서 수정후 재부팅
+     */
+    private fun getQueName(id : Int) = this.queuePrefix + "_" + (id % this.getQueueSize())
 }
