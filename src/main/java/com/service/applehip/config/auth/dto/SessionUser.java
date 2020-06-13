87af813ca4fcf156952039f07cb44706f5f0064b
n1tjrgns <n2tjrgns@gmail.com>
2020-06-13 15:02:20 +0900

    3vs500-42 부트스트랩 추가, 로컬 h2 DB로 전환

--- src/main/java/com/service/applehip/config/auth/dto/SessionUser.java
+++ src/main/java/com/service/applehip/config/auth/dto/SessionUser.java
@@ -1,6 +1,7 @@
 package com.service.applehip.config.auth.dto;
 
 import com.service.applehip.domain.users.GoogleUser;
+import com.service.applehip.domain.users.Users;
 import lombok.Getter;
 
 import java.io.Serializable;
@@ -11,9 +12,16 @@ public class SessionUser implements Serializable {  //인증된 사용자의 정
     private String email;
     private String picture;
 
+
+    //TODO 구글 엔티티 -> Users 엔티티
     public SessionUser(GoogleUser googleUser){
         this.name = googleUser.getName();
         this.email = googleUser.getEmail();
         this.picture = googleUser.getPicture();
+    }
+    public SessionUser(Users user){
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
     }
 }
